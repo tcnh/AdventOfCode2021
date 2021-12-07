92c2646cc5dc52c66c4daaad34208d547baeccfa
Tcnh <tom.heintzberger@gmail.com>
2021-12-07 09:26:27 +0100

    use abs for readability

--- src/com/github/tcnh/aoc/Day7.kt
+++ src/com/github/tcnh/aoc/Day7.kt
@@ -1,7 +1,7 @@
 package com.github.tcnh.aoc
 
 import java.io.File
-import kotlin.math.absoluteValue
+import kotlin.math.abs
 import kotlin.math.floor
 
 val day7input: List<Int> = File("input_day7").readLines()[0].split(",").map { it.toInt() }
@@ -10,13 +10,13 @@ fun main() {
     //Part 1:
     val median = day7input.sorted()[(day7input.size + 1) / 2]
     var part1Consumption = 0
-    day7input.forEach { i -> part1Consumption += (i - median).absoluteValue }
+    day7input.forEach { part1Consumption += abs((it - median)) }
     println(part1Consumption)
 
     //Part 2
-    val average = floor(day7input.average())
+    val average = floor(day7input.average()).toInt()
     var part2Consumption = 0
-    day7input.forEach { i -> part2Consumption += ((((i - average).absoluteValue + 1) / 2) * (i - average).absoluteValue).toInt() }
+    day7input.forEach { part2Consumption += (((abs(it - average) + 1) / 2) * abs(it - average)) }
     println(part2Consumption)
 }
 
